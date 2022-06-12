4d4e8817c5f2ae3fc6ba5553a7dbad781c17d6b4
Robert Soriano <sorianorobertc@gmail.com>
2022-06-11 23:18:24 -0700

    fix: middleware path

--- src/module.ts
+++ src/module.ts
@@ -1,5 +1,7 @@
-import { addTemplate, defineNuxtModule, useLogger } from '@nuxt/kit'
+import { fileURLToPath } from 'url'
+import { addServerHandler, addTemplate, defineNuxtModule, useLogger } from '@nuxt/kit'
 import type { Options } from 'http-proxy-middleware'
+import { join } from 'pathe'
 import { defu } from 'defu'
 // @ts-expect-error: No types
 import dedent from 'dedent'
@@ -16,17 +18,27 @@ export default defineNuxtModule<Options>({
     changeOrigin: true,
   },
   setup(options, nuxt) {
+    const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
+    nuxt.options.build.transpile.push(runtimeDir, '#build/proxy-handler')
+
+    const handlerPath = join(nuxt.options.buildDir, 'proxy-handler.ts')
+
     // Final resolved configuration
     const finalConfig = nuxt.options.runtimeConfig.proxy = defu(nuxt.options.runtimeConfig.proxy, {
       logger: options.logger,
       changeOrigin: options.changeOrigin,
     })
 
+    addServerHandler({
+      handler: handlerPath,
+      middleware: true,
+    })
+
     addTemplate({
-      src: 'proxyMiddleware.mjs',
+      src: 'proxy-handler.ts',
       write: true,
       getContents: () => dedent`
-        import { createProxyMiddleware } from 'http-proxy-middleware'
+        import createProxyMiddleware from '#build/proxy-handler'
         
         export default createProxyMiddleware(${finalConfig})
       `,
--- src/runtime/server.ts
+++ src/runtime/server.ts
@@ -1,15 +1,18 @@
-// ~/server/middleware/proxy.ts
-import { defineEventHandler } from 'h3'
-// @ts-expect-error: Resolved by Nuxt
-import apiProxyMiddleware from '#build/proxyMiddleware.mjs'
+import type { Options } from 'http-proxy-middleware'
+import { createProxyMiddleware as _createProxyMiddleware } from 'http-proxy-middleware'
 
-export default defineEventHandler(async (event) => {
-  await new Promise((resolve, reject) => {
-    apiProxyMiddleware(event.req, event.res, (err?: unknown) => {
-      if (err)
-        reject(err)
-      else
-        resolve(true)
+export default function createProxyMiddleware(options: Options) {
+  const proxyMiddleware = _createProxyMiddleware(options)
+
+  // @ts-expect-error: Resolved by Nuxt
+  return defineEventHandler(async (event) => {
+    await new Promise((resolve, reject) => {
+      proxyMiddleware(event.req, event.res, (err?: unknown) => {
+        if (err)
+          reject(err)
+        else
+          resolve(true)
+      })
     })
   })
-})
+}
