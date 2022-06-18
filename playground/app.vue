edc432ca00d39c5869fb49e3221be6df1d03c3e7
Robert Soriano <sorianorobertc@gmail.com>
2022-06-17 20:56:50 -0700

    update playground

--- playground/app.vue
+++ playground/app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    Hello world
+    <NuxtPage />
   </div>
 </template>
---
+++ playground/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { data } = useFetch<{ id: number; title: string }[]>('http://localhost:3000/api/todos')
+</script>
+
+<template>
+  <ul>
+    <li v-for="t in data" :key="t.id">
+      {{ t.title }}
+    </li>
+  </ul>
+</template>
