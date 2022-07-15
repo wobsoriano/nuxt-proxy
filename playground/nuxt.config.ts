48edf20c4c76c5fac132a6d8ca972bdb5ac6cf4a
Robert Soriano <sorianorobertc@gmail.com>
2022-07-14 21:06:00 -0700

    update nuxt config

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,8 +1,9 @@
 import { defineNuxtConfig } from 'nuxt'
+import Module from '../src/module'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['nuxt-proxy'],
+  modules: [Module],
   proxy: {
     options: [
       {
